bb4d38d8481e50988fc0f47cd27b8073a50a1ce4
Jose Santos <jsmpereira@gmail.com>
2013-11-22 16:00:56 +0900

    Initial commit.

---
+++ public/js/controllers.js
@@ -0,0 +1,50 @@
+var api = angular.module('api', []);
+
+api.config(function($httpProvider) {
+	//Enable cross domain calls
+  $httpProvider.defaults.useXDomain = true;
+
+	//Remove the header used to identify ajax call  that would prevent CORS from working
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+  // Allow POST
+  $httpProvider.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+});
+
+api.controller('APIController', ['$scope', '$http',
+  function ($scope, $http) {
+
+  	$scope.API_ROOT = 'http://limitless-tundra-1502.herokuapp.com/companies';
+
+  	$scope.index = function() {
+  		$http.get(this.API_ROOT).success(function(data) {
+    		console.log("200 OK", data)
+      	$scope.companies = data;
+    	});
+  	}
+
+  	$scope.show = function(id) {
+  		$http.get(this.API_ROOT+"/"+id).success(function(data) {
+    		console.log("200 OK", data)
+      	$scope.company = data;
+    	});
+  	}
+
+		$scope.post = function(data) {
+			// Angular just POSTs JSON. Need to use jQuery's $.param.
+  		$http.post(this.API_ROOT, $.param({company: data}) ).success(function(result) {
+    		console.log("200 OK", result)
+    		if (result.status == 400) {
+    			window.alert("Please, check your input.")
+    		} else {
+    			window.alert("Company Created.")
+    			$scope.index()	
+    		}
+    	});
+  	}
+
+  	angular.element(document).ready(function () {
+        $scope.index();
+    });
+
+  }]);
---
+++ public/js/services.js
@@ -0,0 +1,10 @@
+//var apiService = angular.module('apiService', ['ngResource']);
+//
+//var API_INDEX = 'http://localhost:3000/companies';
+//
+//apiService.factory('API', ['$resource',
+//  function($resource){
+//    return $resource(API_INDEX, {}, {
+//      index: {method:'GET'}
+//    });
+//  }]);
